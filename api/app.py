8783d8860317346d5c59e32ca059cb76c688b1f1
NafisAshraf <nafis.ashraf.roudra@g.bracu.ac.bd>
2023-07-20 23:33:16 +0600

    updated app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,47 @@
 from flask import Flask
 
+from dotenv import load_dotenv
+import os
+import MySQLdb
+
+
 app = Flask(__name__)
+load_dotenv()
+
+
+# connection = MySQLdb.connect(
+#   host= os.getenv("HOST"),
+#   user=os.getenv("USERNAME"),
+#   passwd= os.getenv("PASSWORD"),
+#   db= os.getenv("DATABASE"),
+#   autocommit = True,
+#   ssl_mode = "VERIFY_IDENTITY"
+#   ssl      = {
+#     "ca": "/etc/ssl/cert.pem"
+#   }
+# )
+
+
+
+import mysql.connector as sql
+
+connection = sql.connect(host="aws.connect.psdb.cloud",
+                   database="mydb",
+                   user="bo1sifju3j4baqiwvd1z",
+                   password="pscale_pw_fiVes8i7umh1GKl979nh0kgopinsnYGfifwngf9PUhr",
+                    )
+
+
 
 @app.route('/')
 def index():
-    return 'Hello World !'
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM students;")
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
-
