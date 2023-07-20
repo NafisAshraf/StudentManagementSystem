b7dd05470135f03dedc24a7c6e841c4d84d34107
NafisAshraf <nafis.ashraf.roudra@g.bracu.ac.bd>
2023-07-21 03:18:34 +0600

    used PyMySQL

--- api/app.py
+++ api/app.py
@@ -24,7 +24,7 @@ def index():
                     database=db_name,
                     # ssl_mode="VERIFY_IDENTITY",
                     ssl={
-                        "ca": "/etc/ssl/cert.pem"
+                        "ca": "cert.pem"
                     }
                     )
     cursor = connection.cursor()
